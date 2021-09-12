663623ea0fd7e659940f283885d2db57293ad02a
dhadbs2 <dhadbs2@naver.com>
2021-09-12 22:42:44 +0900

    [update] useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const onSubmit = (event) => {
-    event.preventDefault();//submit이 페이지를 리프레쉬 시키는 것을 방지
-    console.log(username, password);
-  }
+  const [count, setCount] = useState(0);
+  const [Manroo, setManroo] = useState(0);
+
+  useEffect(() => {
+    console.log(count)
+  }, [count])
   
+  const increment = () => {
+    setCount(count + 1);
+  }
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-      <input
-       placeholder="Username"
-       value={username}
-       onChange={(e) => setUsername(e.target.value)} 
-      /><br/>
-      <input
-       placeholder="Password"
-       value={password}
-       onChange={(e) => setPassword(e.target.value)}
-      /><br/>
-      <button type="submit">Login</button>
-      </form>
+      g2
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setManroo(Manroo + 1)}>Click1</button>
     </div>
   );
 }
