05c23140aa66a54cab2525122f0de6fb45386ddf
dhadbs2 <dhadbs2@naver.com>
2021-09-13 22:07:25 +0900

    [update] component 활용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [Manroo, setManroo] = useState(0);
-
-  useEffect(() => {
-    console.log(count)
-  }, [count])
-  
-  const increment = () => {
-    setCount(count + 1);
-  }
 
   return (
     <div className="App">
-      g2
-      <button onClick={increment}>Click</button>
-      <button onClick={() => setManroo(Manroo + 1)}>Click1</button>
-    </div>
+      <h1>hi</h1>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+  </div>
   );
 }
 
